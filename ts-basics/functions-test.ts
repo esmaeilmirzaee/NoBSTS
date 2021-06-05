6a70f098342181883444f58591739a9f8968954d
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-06-05 12:35:53 +0430

    Day 1- basic types and functions

---
+++ ts-basics/functions-test.ts
@@ -0,0 +1,6 @@
+import { addNumbers, getName, joinNames } from './functions';
+
+console.log(addNumbers(1, 2));
+console.log(joinNames('a', 'b'));
+
+console.log(getName({ first: 'Esmaeil', last: 'MIRZAEE' }));
