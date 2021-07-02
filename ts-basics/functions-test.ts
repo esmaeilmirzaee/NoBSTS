9ad23f3c450dd228c43b7772dfb8af783f39d221
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-07-02 16:46:53 +0430

    Init

--- ts-basics/functions-test.ts
+++
@@ -1,8 +0,0 @@
-import { addNumbers, getName, joinNames, salutation } from './functions';
-
-console.log(addNumbers(1, 2));
-console.log(joinNames('a', 'b'));
-
-console.log(getName({ first: 'Esmaeil', last: 'MIRZAEE' }));
-
-console.log(salutation('Hi', 'Esmaeil', 'or', 'Sam'));
