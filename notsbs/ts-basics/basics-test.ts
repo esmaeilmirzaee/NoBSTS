6ca693e4ff640a992b2c28cd0f27adeafb035dc0
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-07-10 21:10:49 +0430

    optional chaining

--- notsbs/ts-basics/basics-test.ts
+++ notsbs/ts-basics/basics-test.ts
@@ -1,4 +1,14 @@
-import { printIngredient } from './basics';
+import { getEmail, getEmailCoercingWay, printIngredient, User } from './basics';
 
 printIngredient('1C', 'Flour');
 printIngredient('1C', 'Flour', 'Water');
+
+let u: User = {
+    name: 'Esmaeil',
+    info: {
+        email: 'e@e.com',
+    },
+};
+
+console.log(getEmail(u));
+console.log(getEmailCoercingWay(u));
--- notsbs/ts-basics/basics.ts
+++ notsbs/ts-basics/basics.ts
@@ -5,3 +5,22 @@ export function printIngredient(
 ): void {
     console.log(`${quantity} ${ingredient} ${extra ? extra : ''}`);
 }
+
+export interface User {
+    name: string;
+    info?: {
+        email?: string;
+    };
+}
+
+// Type 'undefined' is not assignable to type 'string'.
+export const getEmail = (user: User): string => {
+    if (user.info) {
+        return user.info.email!;
+    }
+    return '';
+};
+
+export const getEmailCoercingWay = (user: User): string => {
+    return user?.info?.email ?? '';
+};
