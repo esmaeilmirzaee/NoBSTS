fd48bf229c65789b9c11b1e5e77ff8cac172c2b0
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-07-12 16:26:12 +0430

    optional types

--- notsbs/ts-basics/basics-test.ts
+++ notsbs/ts-basics/basics-test.ts
@@ -1,1 +1,6 @@
+import { updateUser } from './basics';
+
 // Utility types
+console.log(updateUser({ id: 1, name: 'Esmaeil' }, {}));
+console.log(updateUser({ id: 2, name: 'Esmaeil', email: 'e@e.com' }, {}));
+console.log(updateUser({ id: 1, name: 'Esmaeil' }, { email: 'e@example.com' }));
--- notsbs/ts-basics/basics.ts
+++ notsbs/ts-basics/basics.ts
@@ -1,1 +1,12 @@
 // Utility types
+interface User {
+    id: number;
+    name: string;
+    email?: string;
+}
+
+type userOptionals = Partial<User>;
+
+export const updateUser = (u: User, update: userOptionals): User => {
+    return { ...u, ...update };
+};
