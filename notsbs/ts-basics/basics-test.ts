cbffd3a5f0fa7fa5f6fd8943ca263137505af04f
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-07-12 14:19:24 +0430

    end

--- notsbs/ts-basics/basics-test.ts
+++ notsbs/ts-basics/basics-test.ts
@@ -1,6 +1,24 @@
-import { useState } from './basics';
+import { Ranker, useState } from './basics';
 
 const [toggle, setToggle] = useState<boolean>(false);
 console.log(toggle());
 setToggle(true);
 console.log(toggle());
+
+let [darkMode, setDarkMode] = useState<string | null>(null);
+console.log(darkMode());
+setDarkMode('dark mode enabled');
+console.log(darkMode());
+
+interface Pokemon {
+    name: string;
+    hp: number;
+}
+
+const pokemon: Pokemon[] = [
+    { name: 'Bulbasour', hp: 20 },
+    { name: 'Megaasour', hp: 5 },
+];
+
+const ranks = Ranker(pokemon, ({ hp }) => hp);
+console.log(ranks);
--- notsbs/ts-basics/basics.ts
+++ notsbs/ts-basics/basics.ts
@@ -7,3 +7,19 @@ export function useState<T>(initialValue: T): [() => T, (v: T) => void] {
         },
     ];
 }
+
+interface Rank<T> {
+    item: T;
+    rank: number;
+}
+
+export function Ranker<T>(items: T[], rank: (v: T) => number): T[] {
+    const ranks: Rank<T>[] = items.map((item) => ({
+        item,
+        rank: rank(item),
+    }));
+
+    ranks.sort((a, b) => a.rank - b.rank);
+
+    return ranks.map((rank) => rank.item);
+}
