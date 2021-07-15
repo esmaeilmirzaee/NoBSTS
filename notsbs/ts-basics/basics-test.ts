c0e2ae2e7e52d4fa1824d8cfc21c65efdc8c9795
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-07-16 00:06:21 +0430

    #15 readony property

--- notsbs/ts-basics/basics-test.ts
+++ notsbs/ts-basics/basics-test.ts
@@ -1,1 +1,15 @@
+import { User } from './basics';
+
 // Readonly in class
+let admin: User = new User(1, 'admin', 1000);
+console.log(admin);
+admin.groupId = 2;
+
+// Cannot assign to 'name' because it is a read-only property.
+// admin.id = 2;
+
+// Cannot assign to 'name' because it is a read-only property.
+// admin.name = 'not admin';
+
+// Property 'isAdmin' is protected and only accessible within class 'User' and its subclasses.
+// admin.isAdmin = false;
--- notsbs/ts-basics/basics.ts
+++ notsbs/ts-basics/basics.ts
@@ -1,1 +1,9 @@
 // Readonly in class
+export class User {
+    constructor(
+        public readonly id: number,
+        public readonly name: string,
+        public groupId: number,
+        protected isAdmin: boolean = false,
+    ) {}
+}
