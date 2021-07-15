2e84f443289cacca4b14299752fdc4136bc6debe
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-07-15 17:56:01 +0430

    end

--- notsbs/ts-basics/basics-test.ts
+++ notsbs/ts-basics/basics-test.ts
@@ -2,16 +2,16 @@
 
 import { InMemoryDatabase, PersistentDatabase } from './basics';
 
-let myDb = new InMemoryDatabase<string, string>();
-myDb.set('1', 'one');
-console.log(myDb.get('1'));
+let myDb = new InMemoryDatabase<number, string>();
+myDb.set(1, 'one');
+console.log(myDb.get(1));
 // The following line happens because our db is not protected or private.
 // myDb.db['1'] = 'two';
-console.log(myDb.get('1'));
+console.log(myDb.get(1));
 
-let myDB = new PersistentDatabase();
-myDB.set('2', 'two');
+let myDB = new PersistentDatabase<number, string>();
+myDB.set(2, 'two');
 let savedData = myDB.storeDatabase();
 console.log(myDB.storeDatabase());
 myDB.restoreDatabase(savedData);
-console.log(myDB.get('2'));
+console.log(myDB.get(2));
